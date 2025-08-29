c606e4d725b1733f8a310d16af9ac90587858e78
PhotonPotato <photonpotato21@gmail.com>
2025-08-28 15:59:24 -1000

    Working Console, Lobby Creation, Lobby Listing

---
+++ SteamworksTestServer/Assets/Scripts/Console.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public sealed class Console : MonoBehaviour
+{
+    public static Console Instance = null;
+
+    public TMP_Text ConsoleText;
+
+    public void Awake()
+    {
+        if (Instance == null) Instance = this;
+
+        if (Instance == this)
+        {
+            // Clean the console off rip
+            ClearConsole();
+        }
+    }
+
+    public static void ClearConsole()
+    {
+        Instance.ConsoleText.text = "Console:";
+    }
+
+
+    /// <summary>
+    /// Logs an object message to the in-canvas console.
+    /// </summary>
+    /// <param name="message">Object containing message.</param>
+    public static void Log(object message)
+    {
+        if (Instance == null) return;
+
+        Instance.ConsoleText.text += $"\n{Mathf.Round(Time.time * 100) / 100}:\t {message}";
+    }
+}
--- SteamworksTestServer/Assets/Scripts/SteamManager.cs
+++ SteamworksTestServer/Assets/Scripts/SteamManager.cs
@@ -1,33 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Steamworks;
+using Steamworks.Data;
 using UnityEngine;
 
 public class SteamManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class PlayerInfo
+    {
+        public string name { get; private set; }
+        public SteamId steamId { get; private set; }
+
+        public PlayerInfo(string name, SteamId id)
+        {
+            this.name = name;
+            steamId = id;
+        }
+    }
+    public static SteamManager Instance = null;
+
     public uint appid = 480;
+
+    [SerializeField] private PlayerInfo playerInfo;
 
     private void Awake()
     {
-        DontDestroyOnLoad(this);
+        Console.Log("Awake");
 
-        try
+        if (Instance == null) Instance = this;
+
+        // Validate that this is the singleton
+        if (Instance == this)
         {
+            DontDestroyOnLoad(this);
+
             // Try to perform the initial handshake
-            Steamworks.SteamClient.Init(appid, true);
-            Log("Steam is up and running!");
+            try
+            {
+                SteamClient.Init(appid, true);
+                Log("Steam is up and running!");
+            }
+            catch (System.Exception e)
+            {
+                Log(e.Message);
+            }
+
+            // Gather this player info
+            playerInfo = new PlayerInfo
+            (
+                SteamClient.Name,
+                SteamClient.SteamId
+            );
+
+            Log($"Logged in as {playerInfo.steamId} : {playerInfo.name}");
+
+
         }
-        catch (System.Exception e)
+        else
         {
-            Log(e.Message);
+            // We only want one SteamManager
+            Destroy(gameObject);
         }
     }
+
+
+    private void Start()
+    {
+        #region Callbacks
+        SteamMatchmaking.OnLobbyCreated += OnLobbyCreatedCallBack;
+        #endregion
+    }
+
+
+    private void Update()
+    {
+        SteamClient.RunCallbacks();
+    }
+
 
     private void OnApplicationQuit()
     {
         try
         {
-            Steamworks.SteamClient.Shutdown();
+            SteamClient.Shutdown();
             Log("Shutdown!");
         }
         catch
@@ -36,11 +93,30 @@ public class SteamManager : MonoBehaviour
         }
     }
 
+
     void OnDestroy()
     {
         SteamClient.Shutdown();
     }
 
-    // Debug.Log Wrapper (I feel like Ima be using htis a lot)
-    public static void Log(string msg) { Debug.Log(msg); }
+
+    /// <summary>
+    /// Debug.Log Wrapper (I feel like Ima be using htis a lot)
+    /// </summary>
+    /// <param name="msg">Message to log.</param>
+    public static void Log(object msg) { Debug.Log(msg); Console.Log(msg); }
+
+
+    /// <summary>
+    /// Subscribes to steam lobby creation callback.
+    /// </summary>
+    /// <param name="result"></param>
+    /// <param name="lobby"></param>
+    void OnLobbyCreatedCallBack(Result result, Lobby lobby)
+    {
+        if (result != Result.OK)
+        {
+            Log($"Lobby creation result {result}, not OK");
+        }
+    }
 }
